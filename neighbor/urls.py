9956948cc0179292d6f3f044dc382edf77b619ba
FidelisMomanyi <fidelismomanyi001@gmail.com>
2022-03-22 14:55:29 +0300

    deployment to heroku

--- neighbor/urls.py
+++ neighbor/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('home', views.home, name='home'),
-    path('', views.signup, name='signup'),
+    path('signup/', views.signup, name='signup'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('profile/', views.profile, name='profile'),
@@ -14,7 +14,7 @@ urlpatterns = [
     path('newbusiness/', views.new_business, name='new_business'),
     path('newpost/', views.new_post, name='new_post'),
     path('newneighborhood/', views.new_neighborhood, name='new_neighborhood'),
-    path('all-neighborhood/', views.all_neighborhood, name='neighborhood'),
+    path('', views.all_neighborhood, name='neighborhood'),
 ]
 
 if settings.DEBUG:
